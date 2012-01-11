6b8628b0f5c0ed8cc4e6a9e813ea0cb19bf91647
Shikhar Rastogi <shikhar.rastogi2@jgmail.com>
2012-01-11 08:02:32 -0600

    adding some generated files

---
+++ src/web/CreateLadderController.java
@@ -0,0 +1,27 @@
+package web;
+
+import model.TeamInfo;
+
+import javax.servlet.RequestDispatcher;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateLadderController extends javax.servlet.http.HttpServlet {
+   protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+      response.setContentType("text/html");
+      String teamSize = request.getParameter("teamSize");
+      List memberList = new ArrayList();
+      for (int i = 1; i <= Integer.parseInt(teamSize); i++) {
+         memberList.add(request.getParameter("team_member_" + i));
+      }
+      TeamInfo teamInfo = new TeamInfo(teamSize, memberList);
+      request.setAttribute("teamInfo", teamInfo);
+      RequestDispatcher view = request.getRequestDispatcher("pairingLadder.jsp");
+      view.forward(request, response);
+   }
+
+   protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+   }
+}
